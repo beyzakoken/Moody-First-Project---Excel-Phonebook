87e4855c1c9cded4e39d6bc3e2f45df8fbc6f1ae
bkoken1 <bkoken1@stu.vistula.edu.pl>
2020-12-16 11:38:22 +0100

    Add project files.

---
+++ Moody First Project - Excel Phonebook/Excel.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Moody_First_Project___Excel_Phonebook
+{
+    class Excel
+    {
+    }
+}
---
+++ Moody First Project - Excel Phonebook/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Moody_First_Project_Excel_Phonebook
+{
+    public class Person
+    {
+        public string Name;
+        public string Surname;
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}

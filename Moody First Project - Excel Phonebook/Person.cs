03b8e946e4b91caecba8d24690b6ad5c72967b42
bkoken1 <bkoken1@stu.vistula.edu.pl>
2020-12-16 11:46:40 +0100

    Implemented view list

---
+++ Moody First Project - Excel Phonebook/Person.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Moody_First_Project___Excel_Phonebook
+{
+    public class Person
+    {
+        public Person(string name, string surname)
+        {
+
+            mName = name;
+
+            mSurname = surname;
+
+        }
+
+        public Person(string v)
+        {
+            this.v = v;
+        }
+
+        public Person()
+        {
+        }
+
+        private string mName;
+
+        private string mSurname;
+        private string v;
+
+        public string Name
+        {
+            get { return mName; }
+            set { mName = value; }
+        }
+
+        public string Surname
+        {
+            get { return mSurname; } 
+            set { mSurname = value; }
+        }
+
+    }
+}
--- Moody First Project - Excel Phonebook/Program.cs
+++ Moody First Project - Excel Phonebook/Program.cs
@@ -1,18 +1,39 @@
 ﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using Moody_First_Project___Excel_Phonebook;
 
 namespace Moody_First_Project_Excel_Phonebook
 {
-    public class Person
+    public class Program
     {
-        public string Name;
-        public string Surname;
-    }
 
-    class Program
-    {
+        
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Please choose the operation");
+            Console.WriteLine("1) View the list");
+            Console.WriteLine("2) Insert new Person");
+            Console.WriteLine("3) Edit Person");
+            Console.WriteLine("4) Delete Person");
+            Console.WriteLine("5) Exit");
+
+
+            Person person = new Person("Beyzanur"); 
+            Person person1 = new Person("Enes");
+            Person person2 = new Person("Saban");
+
+            List<String> Names = new List<String>();
+            Names.Add("Beyza");
+            Names.Add("Elif");
+            Names.Add("Saban");
+
+            foreach (var name in Names)
+            {
+                Console.WriteLine(name);
+            }
         }
     }
 }
+
+    
